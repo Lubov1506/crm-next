3592f2660d290fd7392dc94efad8eb977b0f1682
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-09-03 16:37:18 +0300

    routing

---
+++ src/app/conponents/server-component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface ServerComponentProps {
+  children?: React.ReactNode;
+}
+export default function ServerComponent({ children }: ServerComponentProps) {
+  return (
+    <div>
+      <span>Server Component</span>;{children}
+    </div>
+  );
+}
